b6398f91950e1d1f0626e88de9b4ca066f133edf
Lucas-d-Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2024-11-30 19:53:47 -0300

    Implementação dos endpoints e dos métodos index e show de marca.

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -14,7 +14,8 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        //
+        $marcas = Marca::all();
+        return $marcas;
     }
 
     /**
@@ -47,6 +48,7 @@ class MarcaController extends Controller
      */
     public function show(Marca $marca)
     {
+        return $marca;
     }
 
     /**
