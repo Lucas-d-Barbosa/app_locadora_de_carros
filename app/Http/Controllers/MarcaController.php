1d6b0c917d078ed883d8e29bde4480065a5d2bef
Lucas-d-Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2024-12-01 16:15:19 -0300

    Criando endpoint e o método de atualização de marca

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -71,7 +71,9 @@ class MarcaController extends Controller
      */
     public function update(Request $request, Marca $marca)
     {
-        //
+
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
