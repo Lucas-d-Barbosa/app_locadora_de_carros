e8e4e7318917e48bb0ae6bf39e5cfb73677d763e
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2024-12-16 17:16:12 -0300

    Adicionado o método refresh para atualizar o token JWT

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,7 +33,8 @@ class AuthController extends Controller
 
     public function refresh()
     {
-        return 'refresh';
+        $token = auth('api')->refresh(); // cliente encaminhe um jwt válido
+        return response()->json(['token' => $token]);
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -25,8 +25,8 @@ Route::prefix('v1')->middleware('jwt.auth')->group(function(){
     Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
     Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
     Route::post('me', 'App\Http\Controllers\AuthController@me');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
 });
 
 Route::post('login', 'App\Http\Controllers\AuthController@login');
 Route::post('logout', 'App\Http\Controllers\AuthController@logout');
-Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
