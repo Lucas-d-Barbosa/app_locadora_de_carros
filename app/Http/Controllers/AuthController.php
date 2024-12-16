8f4fb569eb28440c152f8c635ffb363560fa096e
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2024-12-16 16:23:16 -0300

    Configurado o método de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,9 +6,22 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
-        return 'login';
+
+        $credenciais = $request->all(['email', 'password']);
+        // autenticação do usuário (email e senha)
+
+        $token = auth('api')->attempt($credenciais);
+
+        if($token) // Usuário autenticado com sucesso!
+        {
+            // retornar um jwt (Jason Web Token)
+            return response()->json(['token'=> $token], 200);
+        }
+
+        return response()->json(['erro' => 'Usuário ou senha inválido!'], 403);
+     
     }
 
 
