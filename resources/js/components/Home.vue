2b489cf3dcfcd692efe15322865815d557c303b2
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2024-12-30 16:18:25 -0300

    Criação do formulário de pesquisa de marcas

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -3,7 +3,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Dashboard</div>
+                    <div class="card-header">Bem-vindo!</div>
 
                     <div class="card-body">
                         <p>Apresentar indicadores do sistema</p>
